0c4e1ef1179bb71f8f948177ac0d2ae3b189d532
Yumiko Huang <dayumikoda@gmail.com>
2016-10-06 01:51:50 -0700

    added carousel

--- new_version/js/app.js
+++ new_version/js/app.js
@@ -1,5 +1,20 @@
 var xihuang = angular.module('xihuang', ['ui.bootstrap'])
 
 .controller('MainCtrl', function($scope){
-	
-})
+
+	$scope.nextSlide = function(){
+		$carousel_items = $('.carousel_item');
+		$carousel_items.each(function(i){
+			if($(this).hasClass('show')){
+				$(this).removeClass('show');
+				$(this).addClass('hide');
+			}else if($(this).hasClass('hide')){
+				$(this).removeClass('hide');
+				$(this).addClass('show');
+			}
+		});
+	}
+
+});
+
+
